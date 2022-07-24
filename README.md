331dec52d77142d94f3309dc77979dfd0f5e495b
Rustam Mustafin <rustammuus@mail.ru>
2022-07-24 18:21:16 +0300

    CI Fail

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/nsrd6s7f9f8weyw3?svg=true)](https://ci.appveyor.com/project/MONK412/auto2)
--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -20,4 +20,19 @@ public class PostmanTest {
                 .statusCode(200)
                 .body("data", equalTo("ru.netology"));
     }
+
+    @Test
+    void testText() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Привет, меня зовут Рустам")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет, меня зовут Руслан"));
+    }
 }
